488cfff90a7ab2ccb969c3bae6bc743606c32f67
sakkumar <saket.kumar29@gmail.com>
2024-11-26 00:34:21 -0800

    Remove comments

--- pipelinedp4j/main/com/google/privacy/differentialprivacy/pipelinedp4j/api/SparkQueryBuilder.kt
+++ pipelinedp4j/main/com/google/privacy/differentialprivacy/pipelinedp4j/api/SparkQueryBuilder.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright 2024 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.google.privacy.differentialprivacy.pipelinedp4j.api
 
 import org.apache.spark.sql.Dataset
@@ -30,7 +14,7 @@ internal constructor(data: Dataset<T>, privacyIdExtractor: (T) -> String) :
     privacyIdExtractor = privacyIdExtractor,
   ) {
   /**
-   * Groups the data by keys (corresnponds to groupBy operation in SQL).
+   * Groups the data by keys (corresponds to groupBy operation in SQL).
    *
    * @param groupKeyExtractor a function to extract the group key from the input.
    * @param maxGroupsContributed the maximum number of groups that a single privacy unit can
